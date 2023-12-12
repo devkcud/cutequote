f41c535e3c1267909002b23e9b22f8da3baa9587
André devkcud Albanese <patommmmm@proton.me>
2023-12-12 01:31:15 -0300

    add middleware

--- main.go
+++ main.go
@@ -111,9 +111,27 @@ type Quote struct {
 	Emoji  string `json:"emoji"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	r.Use(CORSMiddleware())
 
 	r.GET("/", getQuotes)
 
